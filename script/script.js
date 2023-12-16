7c4b9197ece4be0ed6b2769f49c3373ea20b3841
Jaobast <153824183+Jaobast@users.noreply.github.com>
2023-12-16 15:33:05 +0100

    git init

---
+++ script/script.js
@@ -0,0 +1,18 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+function checkGuess() {
+  let inputElement = document.getElementById('guess');
+  let feedbackElement = document.getElementById('feedback');
+  let guess = parseInt(inputElement.value);
+
+  if (guess === randomNumber) {
+    feedbackElement.innerHTML = "good girl!";
+    feedbackElement.style.color = "green";
+  } else if (guess < randomNumber) {
+    feedbackElement.innerHTML = "Too low! Try again.";
+    feedbackElement.style.color = "red";
+  } else {
+    feedbackElement.innerHTML = "Too high! Try again.";
+    feedbackElement.style.color = "red";
+  }
+}
