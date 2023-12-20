babd041ccf2a2ea119ed6347db6b1741587e4b53
Jaobast <153824183+Jaobast@users.noreply.github.com>
2023-12-20 22:36:44 +0100

    js

--- script/script.js
+++ script/script.js
@@ -1,20 +1,26 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+let randomNumber = generateRandomNumber();
+
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
 
 function checkGuess() {
   let inputElement = document.getElementById('display');
   let feedbackElement = document.getElementById('feedback');
-  let display = parseInt(inputElement.value);
+  let display = inputElement.textContent.trim();
 
-  if (display === randomNumber) {
-    feedbackElement.innerHTML = "good girl!";
+  if (display === randomNumber.toString()) {
+    feedbackElement.innerHTML = "Good guess!";
     feedbackElement.style.color = "green";
-  } else if (display < randomNumber) {
+  } else if (parseInt(display) < randomNumber) {
     feedbackElement.innerHTML = "Too low! Try again.";
     feedbackElement.style.color = "red";
   } else {
     feedbackElement.innerHTML = "Too high! Try again.";
     feedbackElement.style.color = "red";
   }
+
+  clearDisplay(); 
 }
 
 let display = document.getElementById('display');
